a8c7c6381aea22a579c658d094ca4865ebaca915
lulzmodder <lulzmodder@gmail.com>
2015-07-07 21:12:22 +0530

    Create hngweapon.cpp

---
+++ src/hngweapon.cpp
@@ -0,0 +1,41 @@
+
+/*
+    The code should not be re-ditributed without the permission of lulzmodder
+    Copyright (C) 2015  lulzmodder
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    */
+    
+#include "hngweapon.h"
+
+DWORD GET_WEAPON_HASH(std::string name)
+{
+	for (int i = 0; i < 62; i++)
+		if (weapon_array[i][0].compare(name) == 0)
+			return std::stol(weapon_array[i][0]);
+	return -1;
+}
+
+void GET_WEAPON_NAME(std::string Hash, std::string& name)
+{
+	for (int i = 0; i < 62; i++)
+	{
+		if (weapon_array[i][1].compare(Hash) == 0)
+		{
+			name = weapon_array[i][0];
+			break;
+		}
+	}
+}
